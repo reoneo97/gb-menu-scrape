a169e43d24435b827141ee06267a3f9cfe9f97d8
Reo Neo <reoneo97@gmail.com>
2022-06-25 12:10:45 +0800

    edited to use click and ask for user input

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scraper 🐳
+
+## Installation
+
+Install Poetry: `curl -sSL https://install.python-poetry.org | python3 -`
+
+Run:
+
+```sh
+poetry install # Install Dependencies
+poetry run python scrape.py # Run Script
+```
--- scrape.py
+++ scrape.py
@@ -2,27 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import click
+from datetime import date, datetime
+import os
 
 HEADERS={
     'Referrer-Policy':'origin-when-cross-origin',
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36"
 }
-def main(url):
+
+@click.command()
+def main():
+    click.echo("This is the Grab Scraper 🐳🐳🐳")
+    click.echo('Enter the URL below. It should be similar to https://food.grab.com/my/en/restaurant/burger-king-plaza-low-yat-delivery/1-CYUVMB3DUEABUE')
+    url = click.prompt('URL',type=str)
+    click.echo('Running the Scraper')
+    scrape(url)
+
+def dt_now():
+    return datetime.now().strftime('%d-%m-%y-%H-%M')
+
+def scrape(url):
+
     req = requests.get(url,headers = HEADERS)
-    # with open('hi.html','w+') as f:
-    #     f.write(req.text)
-    # print(req)
+    if req.status_code != 200:
+        raise ConnectionError('Unable to access URL')
     soup = BeautifulSoup(req.text, 'html.parser')
-    print(soup.prettify()[:1000])
-    df = []
-    # print(soup)
-
-
+    click.secho("Webpage Accessed",fg='cyan')
+    
     menu_div = soup.find('div',class_=re.compile('menuContentWrapper*'))# 4 is the index of the container
-
-
     categories = menu_div.find_all('div',class_=re.compile('category*'))
-    print(f'{len(categories)} categories found')
+    
+    click.secho("Categories Identified",fg='cyan')
+    
+    df = []
     for cat in categories:
         category_name = cat.find('h2',class_=re.compile('categoryName'))
         if category_name: # Some weird interaction which creates duplicatess
@@ -49,13 +62,17 @@ def main(url):
                 'price': item_price,
                 'description': item_desc,
             }
+
             df.append(info)
 
     df = pd.DataFrame.from_dict(df)
     restaurant_name = url.split('/')[-2]
     df = df.drop_duplicates()
-    df.to_csv(f'{restaurant_name}.csv',index=False)
+    if not os.path.isdir('output'):
+        os.mkdir('output')
+    df.to_csv(f'output/{restaurant_name}_{dt_now()}.csv',index=False)
+    click.secho("Data Saved",fg='cyan')
+
     
 if __name__ == "__main__":
-    url = "https://food.grab.com/my/en/restaurant/burger-king-plaza-low-yat-delivery/1-CYUVMB3DUEABUE"
-    main(url)
+    main()
